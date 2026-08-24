f5f0f73eda95ef60c30734f0a6847c16b4063e04
Diego22182026 <00242126@uca.edu.sv>
2026-08-24 11:36:30 -0600

    feat: add branches and struct

---
+++ agenda.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Contacto{
+
+    string Nombre, telefono, email;
+};
+
+const int contactos_max = 100;
+Contacto Agenda[contactos_max];
+
+int main (){
+
+
+    return 0;
+}
