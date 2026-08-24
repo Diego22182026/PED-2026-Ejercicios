6a71a9ae3a7899191478108edc9a6f764d3b9aca
Diego22182026 <00242126@uca.edu.sv>
2026-08-24 11:41:03 -0600

    feat: add functions for agenda

--- agenda.cpp
+++ agenda.cpp
@@ -3,12 +3,28 @@ using namespace std;
 
 struct Contacto{
 
-    string Nombre, telefono, email;
+    string nombre, telefono, email;
 };
 
 const int contactos_max = 100;
 Contacto Agenda[contactos_max];
 
+void mostrarAgenda(Contacto agenda[], int n){
+    cout << "Agenda de contactos: " << endl;
+
+    if (n = 0){
+        cout << "No hay contactos " << endl;
+        return;
+    }
+
+    for (int i = 0; i < n; i++){
+        cout << "Nombre: " << agenda[i].nombre << endl;
+        cout << "Telefono " << agenda[i].telefono << endl;
+        cout << "Email " << agenda[i].email << endl;
+    }
+
+
+}
 int main (){
 
 
